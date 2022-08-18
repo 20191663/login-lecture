7afc239088264437a44c8245e7c5c7cc57d26edb
20191663 <ihs20191663@gmail.com>
2022-08-19 02:01:19 +0900

    fetch, API

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,21 @@
 "use strict";
 
-console.log("Hello, express!");
+const id = document.querySelector("#id"),
+  pw = document.querySelector("#pw"),
+  loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+function login() {
+  const req = {
+    id: id.value,
+    pw: pw.value,
+  };
+
+  fetch("/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(req),
+  });
+}
