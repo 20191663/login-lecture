de5efa3d57fa466b3c836c8875893a8c6b2690ad
20191663 <ihs20191663@gmail.com>
2022-08-19 03:55:50 +0900

    로그인 기능 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -10,9 +10,27 @@ const output = {
   },
 };
 
+const users = {
+  id: ["kim", "park", "choi"],
+  pw: ["1234", "1q2w3e4r", "iloveyou"],
+};
+
 const process = {
   login: (req, res) => {
-    console.log(req.body);
+    const id = req.body.id,
+      pw = req.body.pw;
+    if (users.id.includes(id)) {
+      const idx = users.id.indexOf(id);
+      if (users.pw[idx] === pw) {
+        return res.json({
+          success: true,
+        });
+      }
+    }
+    return res.json({
+      success: false,
+      msg: "로그인에 실패하셨습니다.",
+    });
   },
 };
 
